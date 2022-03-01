3e75924cccc7e43ad9cece6d4968c5ca0711d199
Nguyễn Thị Hiền Lương <conmuangangqua200x@gmail.com>
2022-03-01 16:35:14 +0700

    create model again

---
+++ HotelManagement/src/java/model/Customer.java
@@ -0,0 +1,57 @@
+package model;
+
+/**
+ *
+ * @author Tom
+ */
+public class Customer {
+
+    private int customerID;
+    private String customerName;
+    private String phone;
+    private String email;
+    private String address;
+
+    public Customer() {
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
